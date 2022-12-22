f8bb61aee1bca416cd44b10c4ce73e60024f68bf
yuval weiss <yuvalweiss1403@gmail.com>
2022-12-22 09:23:28 +0200

    day 8 update

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ function handleClick(){
 function delayHandleClick(){
     setTimeout(handleClick , 3000);
 }
+
+let checkIfClicked = false;
+  
+  function switchmode(event) {
+    var element = document.getElementById("Switch-Mode");
+    if(!checkIfClicked){
+            //change to dark mode
+            element.classList.add("dark-mode");
+            //check if clicked = true
+            checkIfClicked = true;
+    }else{
+        //change to light mode
+        element.classList.remove("dark-mode");
+        //check if clicked = false
+        checkIfClicked = false;
+
+
+    }
+  }
