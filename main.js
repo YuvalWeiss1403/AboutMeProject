4b9a7b2aee35b6a1cc865bf630e36094f43b0d05
yuval weiss <yuvalweiss1403@gmail.com>
2023-01-02 10:09:28 +0200

    DOM

--- main.js
+++ main.js
@@ -40,3 +40,27 @@ let element = document.body;
   
 
   
+
+  
+const aboutMePictures ={
+  pictures:[
+  {class:"Left-picture" , source:"/assets/computer-programming.jpeg", alt:"computer-programming", description:"My course"},
+  {class:"Middle-picture" , source:"/assets/dance.jpg", alt:"dance", description:"My hobbie"},
+  {class:"Right-picture" , source:"/assets/nails.jpg", alt:"nails", description:"My side Job"}]
+}
+for (let i = 0; i < aboutMePictures.pictures.length; i++) {
+  const element = aboutMePictures.pictures[i];
+  let img = document.createElement(`img`);
+  img.src = element.source;
+  img.className =element.class;
+  img.alt =element.alt;
+  img.style.display= `inline-Block`;
+  let p = document.createElement(`p`);
+  p.textContent = element.description;
+  p.style.display= `inline-Block`;
+  document.querySelector(`section`).appendChild(img);
+  document.querySelector(`section`).appendChild(p);
+  }
+
+
+
